db4935e790038a1da0a441cfabee89775527640b
gavin-shaw <gavin@gnarus.io>
2022-06-09 08:43:45 +0200

    fix: missing dependency in listener container

--- listener/listener_container.py
+++ listener/listener_container.py
@@ -6,6 +6,7 @@ from shared.metabomb_api_service import MetabombApiService
 
 from listener.listener_service import ListenerService
 from listener.notification_service import NotificationService
+from shared.metabomb_coingecko_service import MetabombCoingeckoService
 
 
 class ListenerContainer(BaseContainer):
@@ -16,7 +17,12 @@ class ListenerContainer(BaseContainer):
 
         DISCORD_BASE_URL = config("DISCORD_BASE_URL")
         DISCORD_CHANNEL_ID = config("DISCORD_CHANNEL_ID")
-
+        
+        self.metabomb_coingecko_service = MetabombCoingeckoService(
+            cache_service=self.cache_service,
+            coingecko_service=self.coingecko_service
+        )
+        
         self.metabomb_api_service = MetabombApiService(
             cache_service=self.cache_service
         )
